3ee2d798cbdda3c71b011cd9858fc430063152dc
Will <doggy.huang@gmail.com>
2020-09-16 13:32:11 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/ClientsController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ClientsController : Controller
+    {
+        static List<Client> data = new List<Client>() {
+            new Client() { Id = 1, Name = "Duotify", Phone = "0222221111" },
+            new Client() { Id = 2, Name = "Miniasp", Phone = "0937348483" },
+            new Client() { Id = 3, Name = "SKBank", Phone = "0483743829" },
+        };
+
+        public ClientsController()
+        {
+        }
+
+        // GET: Clients
+        public ActionResult Index()
+        {
+            return View(data);
+        }
+
+        // GET: Clients/Details/5
+        public ActionResult Details(int id)
+        {
+            var item = data.FirstOrDefault(p => p.Id == id);
+
+            return View(item);
+        }
+
+        // GET: Clients/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clients/Create
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            try
+            {
+                data.Add(client);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clients/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Clients/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clients/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/IClient.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public interface IClient
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Phone { get; set; }
+    }
+}
